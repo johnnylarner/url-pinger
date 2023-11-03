801dd8b6bee934f368e51a95c882eda1ee59c380
james.richardson <james.richardson@alexanderthamm.com>
2023-11-03 09:34:05 +0100

    feat: add sync body response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use reqwest::{self, Error};
+
+/// A simple URL pinger that gives you response times and status codes
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct UrlPinger {
+    /// Comma separated list of URLs to ping
+    #[arg(short, long)]
+    urls: String
+}
+
+
+fn main() -> Result<(), Error>  {
+    let pinger = UrlPinger::parse();
+
+    for url in pinger.urls.split(",") {
+        let body = reqwest::blocking::get(url)?
+            .text()?;
+        println!("body = {:?}", body)
+    }
+    Ok(())
+    
 }
